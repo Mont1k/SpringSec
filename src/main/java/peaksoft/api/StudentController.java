7e9f6e3732aadfc852f60483cc1be08b8efc5b2a
Mont1k <nurtilekorozov@gmail.com>
2022-11-29 12:26:44 +0600

    a

--- src/main/java/peaksoft/api/StudentController.java
+++ src/main/java/peaksoft/api/StudentController.java
@@ -2,7 +2,7 @@ package peaksoft.api;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-v
+
 @Controller
 @RequestMapping("/")
 public class StudentController {
